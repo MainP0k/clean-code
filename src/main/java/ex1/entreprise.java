38684c912f3a354bf9ef657aab5dcf7a5f589eb9
P0K <99667896+MainP0k@users.noreply.github.com>
2024-04-23 16:05:26 +0200

    Correction entreprise.java
    Changement fais sur ce fichier : Nom de classe ;  Nom des variables; Modificateurs d'accès pour les variables d'instance; Constantes ; Méthodes et leurs fonctionnalités.

--- src/main/java/ex1/entreprise.java
+++ src/main/java/ex1/entreprise.java
@@ -2,16 +2,58 @@ package ex1;
 
 import java.util.Date;
 
-public class entreprise {
+/**
+ * Classe représentant les informations de base d'une entreprise.
+ */
 
-    public static final int capitalMax = 3000000;
-    public int Siret;
-    public String Nom;
-    public String adresse;
-    public Date date_Creation;
+public class Entreprise {
 
-    public void Afficher_statut() {
+    public static final int CAPITAL_MAX = 3000000;
+    private int siret;
+    private String nom;
+    private String adresse;
+    private Date dateCreation;
 
+    /**
+     * Affiche le statut de l'entreprise.
+     */
+    public void afficherStatut() {
+        System.out.println("Siret: " + siret);
+        System.out.println("Nom: " + nom);
+        System.out.println("Adresse: " + adresse);
+        System.out.println("Date de création: " + dateCreation);
     }
 
+    // Getters et Setters
+    public int getSiret() {
+        return siret;
+    }
+
+    public void setSiret(int siret) {
+        this.siret = siret;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
 }
