51330750e2a4d041d0d24b1159354b5cc5d76af4
P0K <99667896+MainP0k@users.noreply.github.com>
2024-06-24 09:28:03 +0200

    Update Item.java
    Ajout d'un constructeur les objets avec un nom et un poids.
    Ajout de commentaire afin d 'améliorerr la lecture du code.

--- src/main/java/ex5/Item.java
+++ src/main/java/ex5/Item.java
@@ -1,41 +1,55 @@
 package ex5;
 
+/**
+ * Représente un objet avec un nom et un poids.
+ */
 public class Item {
 
     private String nom;
     private int poids;
 
     /**
-     * Getter pour l'attribut nom
+     * Constructeur pour créer un item avec un nom et un poids.
      *
-     * @return the nom
+     * @param nom le nom de l'item
+     * @param poids le poids de l'item
+     */
+    public Item(String nom, int poids) {
+        this.nom = nom;
+        this.poids = poids;
+    }
+
+    /**
+     * Getter pour l'attribut nom.
+     *
+     * @return le nom de l'item
      */
     public String getNom() {
         return nom;
     }
 
     /**
-     * Setter pour l'attribut nom
+     * Setter pour l'attribut nom.
      *
-     * @param nom the nom to set
+     * @param nom le nom à définir pour l'item
      */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
     /**
-     * Getter pour l'attribut poids
+     * Getter pour l'attribut poids.
      *
-     * @return the poids
+     * @return le poids de l'item
      */
     public int getPoids() {
         return poids;
     }
 
     /**
-     * Setter pour l'attribut poids
+     * Setter pour l'attribut poids.
      *
-     * @param poids the poids to set
+     * @param poids le poids à définir pour l'item
      */
     public void setPoids(int poids) {
         this.poids = poids;
