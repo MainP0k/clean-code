3df8c9f6daa83797f31f0c141ef3bbc5b3668eaa
P0K <99667896+MainP0k@users.noreply.github.com>
2024-06-24 08:45:07 +0200

    Update LivretA.java
    Ajout de commentaire pour la lecture de code et ajout d'une exception afin d'être sur que ce sois un livret A

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,24 @@
 package ex2;
 
+/**
+ * Représente un livret A, qui est un type de compte bancaire avec un taux de rémunération.
+ */
 public class LivretA extends CompteBancaire {
 
-    public LivretA(String type, double solde, double tauxRemuneration) {
+    /**
+     * Constructeur pour créer un Livret A avec un solde initial et un taux de rémunération.
+     * @param type de compte verifier que ce sois bien un "Livret A"
+     * @param solde le solde initial du livret A
+     * @param tauxRemuneration le taux de rémunération applicable au livret A
+     */
+    public LivretA(String type; double solde, double tauxRemuneration) {
+        
+        // Appel du constructeur de la classe parente CompteBancaire
+        // Avec le type, le solde initial, 0 comme découvert autorisé (car un Livret A n'a pas de découvert autorisé) et le taux de rémunération
+        // Utilisation du constructeur pour livret A
+        if (!type.equals("LA")) {
+            throw new IllegalArgumentException("Type de compte invalide. Le type doit être 'LA' pour un Livret A.");
+        }
         super(type, solde, 0, tauxRemuneration);
     }
-
 }
