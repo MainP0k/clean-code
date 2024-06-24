0b640b6b4de5278188037a89c82dffd70b4efa3c
P0K <99667896+MainP0k@users.noreply.github.com>
2024-06-24 08:58:50 +0200

    Update Zoo.java
    Ajout des commentaires

--- src/main/java/ex3/Zoo.java
+++ src/main/java/ex3/Zoo.java
@@ -1,7 +1,11 @@
 package ex3;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Représente un zoo avec une liste d'animaux.
+ */
 public class Zoo {
 
     private String nom;
@@ -9,39 +13,62 @@ public class Zoo {
     private List<String> noms;
     private List<String> comportements;
 
+    /**
+     * Constructeur pour créer un zoo avec un nom.
+     *
+     * @param nom le nom du zoo
+     */
     public Zoo(String nom) {
         this.nom = nom;
+        this.types = new ArrayList<>();
+        this.noms = new ArrayList<>();
+        this.comportements = new ArrayList<>();
     }
 
+    /**
+     * Ajoute un animal au zoo.
+     *
+     * @param nomAnimal le nom de l'animal
+     * @param typeAnimal le type de l'animal (ex. mammifère, poisson, etc.)
+     * @param comportement le comportement de l'animal (ex. herbivore, carnivore, etc.)
+     */
     public void addAnimal(String nomAnimal, String typeAnimal, String comportement) {
         types.add(typeAnimal);
         noms.add(nomAnimal);
         comportements.add(comportement);
     }
 
+    /**
+     * Affiche la liste des animaux présents dans le zoo.
+     */
     public void afficherListeAnimaux() {
         for (int i = 0; i < types.size(); i++) {
             System.out.println(noms.get(i) + " " + types.get(i) + " " + comportements.get(i));
         }
     }
 
+    /**
+     * Retourne la taille du zoo (le nombre d'animaux).
+     *
+     * @return le nombre d'animaux dans le zoo
+     */
     public int taille() {
         return types.size();
     }
 
     /**
-     * Getter for nom
+     * Getter pour le nom du zoo.
      *
-     * @return the nom
+     * @return le nom du zoo
      */
     public String getNom() {
         return nom;
     }
 
     /**
-     * Setter
+     * Setter pour le nom du zoo.
      *
-     * @param nom the nom to set
+     * @param nom le nom à définir pour le zoo
      */
     public void setNom(String nom) {
         this.nom = nom;
